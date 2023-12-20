277052278822dec84c4a36bfd56902d21328daf0
motalebjomadar <motalabjomadar@gmail.com>
2023-12-20 21:45:31 +0800

    Add item

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const mainContainer = document.getElementById('text-container');
 
 textBtn.addEventListener('click',function() {
     const comment = textBox.value;
-    textBox.value = '';
     const p = document.createElement('p');
     p.innerText = comment;
     mainContainer.appendChild(p);
+    textBox.value = '';
 })
 
