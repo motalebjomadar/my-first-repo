18a6eeb304530e4a0e87ae8ca68caed8da9f369c
motalebjomadar <motalabjomadar@gmail.com>
2023-12-25 22:18:13 +0800

    Event delegation

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const btn = document.getElementById('delete-btn');
-const textBox = document.getElementById('text-box');
-const title = document.getElementById('section-title');
+const delegateContainer = document.getElementById('delegate-container');
+const items = document.getElementsByClassName('bubble-item');
+const btn = document.getElementById('btn');
 
-btn.addEventListener('click', function() {
-    title.style.visibility = 'hidden';
+// for(const item of items) {delegate-container
+//     item.addEventListener('click', function (event) {
+//         // bubbleContainer.removeChild(item);
+//         event.target.parentNode.removeChild(event.target);
+//     });
+// }
+delegateContainer.addEventListener('click', function (event) {
+    event.target.parentNode.removeChild(event.target);
 })
 
-textBox.addEventListener('keyup', function(event) {
-    if(textBox.value == 'delete') {
-        btn.removeAttribute('disabled');
-    }
-    else{
-        btn.setAttribute('disabled', true);
-    }
-});
+
+btn.addEventListener('click', function() {
+    const li = document.createElement('li');
+    li.classList.add('bubble-item');
+    li.innerText = 'Added new item';
+    delegateContainer.appendChild(li);
+})
+
 
 
 
