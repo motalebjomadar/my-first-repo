9d29548c7b3c5aed92b9e15a0dd45f2779a9dde2
motalebjomadar <motalabjomadar@gmail.com>
2023-12-25 23:42:58 +0800

    Click to add items and click to item for deleling item with js dom

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const delegateContainer = document.getElementById('delegate-container');
-const items = document.getElementsByClassName('bubble-item');
-const btn = document.getElementById('btn');
 
-// for(const item of items) {delegate-container
-//     item.addEventListener('click', function (event) {
-//         // bubbleContainer.removeChild(item);
-//         event.target.parentNode.removeChild(event.target);
-//     });
-// }
-delegateContainer.addEventListener('click', function (event) {
-    event.target.parentNode.removeChild(event.target);
+const addBtn = document.getElementById('btn');
+
+// Create container tag with js dom
+addBtn.addEventListener('click', function() {
+    const item = document.createElement('li');
+    item.classList.add('list-item');
+    item.innerText = 'item added';
+    document.body.appendChild(item);
+
+
+    item.addEventListener('click', function (event) {
+    
+        event.target.parentNode.removeChild(event.target);
+    });
 })
 
 
-btn.addEventListener('click', function() {
-    const li = document.createElement('li');
-    li.classList.add('bubble-item');
-    li.innerText = 'Added new item';
-    delegateContainer.appendChild(li);
-})
+
 
 
 
