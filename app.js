4cfaa452a5da3b4ff2b0f0fb85551c9f881f2c3f
motalebjomadar <motalabjomadar@gmail.com>
2024-01-24 23:30:43 +0800

    practice js functionalities on bank calculation

--- app.js
+++ app.js
@@ -1,21 +1,59 @@
+function getInputValue(inputId){
+    const inputField = document.getElementById(inputId);
+    const getInputText = inputField.value ;
+    const inputNumber = parseFloat(getInputText);
+    inputField.value = '';
+    return inputNumber ;
+}
 
-const addBtn = document.getElementById('btn');
+function getInnerTextValue(fieldId){
+    const fieldTag = document.getElementById(fieldId);
+    const fieldValueInText = fieldTag.innerText ;
+    const value = parseFloat(fieldValueInText);
+    return value ;
+}
 
-// Create container tag with js dom
-addBtn.addEventListener('click', function() {
-    const item = document.createElement('li');
-    item.classList.add('list-item');
-    item.innerText = 'item added';
-    document.body.appendChild(item);
+function updateTotal(totalId, amount){
+    const totalField = document.getElementById(totalId);
+    const getTotalFieldText = totalField.innerText ;
+    const previousFieldNumber = parseFloat(getTotalFieldText);
+    const newTotal = previousFieldNumber + amount ;
+    totalField.innerText = newTotal ;
+    return newTotal;
+}
 
-
-    item.addEventListener('click', function (event) {
+function updateBalance(amount, isAdding ) {
+    const balanceField = document.getElementById('total-balance');
+    const balanceText = balanceField.innerText ;
+    const previousBalance = parseFloat(balanceText);
+    let newBalance;
+    if(isAdding == true ) {
+        newBalance = previousBalance + amount ;
+    }
+    else{
+        newBalance = previousBalance - amount ;
+    }
     
-        event.target.parentNode.removeChild(event.target);
-    });
+    balanceField.innerText = newBalance ;
+}
+
+document.getElementById('deposit-button').addEventListener('click',function(){
+    const amount = getInputValue('deposit-input');
+    if(amount > 0 ) {
+        updateTotal('deposit-total',amount);
+        updateBalance(amount, true);
+    }
+    
 })
-
-
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const amount = getInputValue('withdraw-input');
+    const balance = getInnerTextValue('total-balance');
+    if(amount > 0 & amount <= balance ) {
+        updateTotal('withdraw-total', amount);
+        updateBalance(amount, false);
+    }
+        
+})
 
 
 
